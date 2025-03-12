ed10d45d64b7b2add6977c149f430d8936255951
harvey <harveytseng2@gmail.com>
2025-03-12 08:35:22 -0400

    updates posts to use new Firestore service

--- models/post.py
+++ models/post.py
@@ -1,14 +1,19 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+
 class Post(BaseModel):
-    id: Optional[str]  # This field is optional (populated later)
+    id: Optional[str] = None
     author: str
     content: str
     created_at: str
     likeCount: int = 0
     comments: List[str] = []
+
+
 class LikeRequest(BaseModel):
     user_id: str
+
+
 class CommentRequest(BaseModel):
-    comment: str
+    text: str
