7bfd6bdd2d5e5212824b6cd8f4906892bd1e4ef9
harvey <harveytseng2@gmail.com>
2025-03-30 20:12:32 -0400

    fixes duplicate annotations

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,30 +1,30 @@
 from fastapi import APIRouter
 
+from dependencies import JobPoster, JobVerifier
 from models.job_report import JobReport
-from dependencies import JobPostingService, JobVerificationService
 
 router = APIRouter()
 
 
 @router.post("/report")
-async def create_job_report(report: JobReport, job_service: JobPostingService):
+async def create_job_report(report: JobReport, job_service: JobPoster):
     id = await job_service.post_job(report)
     return {"message": "Job report created successfully with ID: " + id}
 
 
 @router.patch("/verify/{job_id}")
-async def verify_job(job_id: str, verified: bool, report: JobReport, job_service: JobVerificationService):
+async def verify_job(job_id: str, verified: bool, report: JobReport, job_service: JobVerifier):
     await job_service.verify_job(job_id, verified, report)
     return {"message": "Job verified successfully"}
 
 
 @router.delete("/delete/{job_id}")
-async def delete_job(job_id: str, job_service: JobVerificationService):
+async def delete_job(job_id: str, job_service: JobVerifier):
     await job_service.delete_job(job_id)
     return {"message": "Job deleted successfully"}
 
 
 @router.get("/unverified")
-async def get_unverified_jobs(job_service: JobVerificationService):
+async def get_unverified_jobs(job_service: JobVerifier):
     jobs = await job_service.get_unverified_jobs()
     return jobs
